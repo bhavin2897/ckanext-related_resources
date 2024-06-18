cffa919e359249217b81b980527d0d62151f4800
Bhavin Katabathuni <bhavin2897@gmail.com>
2024-06-18 15:38:25 +0200

    test not save relationship

--- ckanext/related_resources/plugin.py
+++ ckanext/related_resources/plugin.py
@@ -16,18 +16,19 @@ class RelatedResourcesPlugin(plugins.SingletonPlugin):
         toolkit.add_resource('fanstatic',
                              'related_resources')
 
-    def get_blueprint(self):
-        blueprint = Blueprint(self.name, self.__module__)
-
-        blueprint.template_folder = u'templates'
-        blueprint.add_url_rule(
-            u'/localhost:5000/fancy_type/<package_name>',
-            u'save_relationships',
-            RelatedController.save_relationships,
-            methods=['POST']
-        )
-        return blueprint
-
-    # ITemplateHelpers
-    def get_helpers(self):
-        return {'related_resources': RelatedController.save_relationships, }
+    #def get_blueprint(self):
+    #    blueprint = Blueprint(self.name, self.__module__)
+    #
+    #    blueprint.template_folder = u'templates'
+    #    blueprint.add_url_rule(
+    #        u'/localhost:5000/fancy_type/<package_name>',
+    #        u'save_relationships',
+    #        RelatedController.save_relationships,
+    #        methods=['POST']
+    #    )
+    #    return blueprint
+    #
+    ## ITemplateHelpers
+    #def get_helpers(self):
+    #    return {'related_resources': RelatedController.save_relationships, }
+    #
