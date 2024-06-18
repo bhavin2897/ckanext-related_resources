7bc1c689883061db2d1007e200f87ebbe019338e
Bhavin Katabathuni <bhavin2897@gmail.com>
2024-06-18 15:45:12 +0200

    revive model

--- ckanext/related_resources/plugin.py
+++ ckanext/related_resources/plugin.py
@@ -1,6 +1,6 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-#from ckanext.related_resources.controllers.related_link import RelatedController
+from ckanext.related_resources.controllers.related_link import RelatedController
 from flask import Blueprint
 
 
@@ -16,19 +16,19 @@ class RelatedResourcesPlugin(plugins.SingletonPlugin):
         toolkit.add_resource('fanstatic',
                              'related_resources')
 
-    #def get_blueprint(self):
-    #    blueprint = Blueprint(self.name, self.__module__)
-    #
-    #    blueprint.template_folder = u'templates'
-    #    blueprint.add_url_rule(
-    #        u'/localhost:5000/fancy_type/<package_name>',
-    #        u'save_relationships',
-    #        RelatedController.save_relationships,
-    #        methods=['POST']
-    #    )
-    #    return blueprint
-    #
+    def get_blueprint(self):
+        blueprint = Blueprint(self.name, self.__module__)
+
+        blueprint.template_folder = u'templates'
+        blueprint.add_url_rule(
+            u'/localhost:5000/fancy_type/<package_name>',
+            u'save_relationships',
+            RelatedController.save_relationships,
+            methods=['POST']
+        )
+        return blueprint
+
     ## ITemplateHelpers
-    #def get_helpers(self):
-    #    return {'related_resources': RelatedController.save_relationships, }
+    def get_helpers(self):
+       return {'related_resources': RelatedController.save_relationships, }
     #
